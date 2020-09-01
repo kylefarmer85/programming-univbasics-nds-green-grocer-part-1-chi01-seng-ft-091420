07372801a6c8dd6ed47c15b923a4d300882aadda
Kyle Farmer <kylefarmer85@gmail.com>
2020-09-01 19:15:46 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -2,7 +2,7 @@ require 'pry'
 def find_item_by_name_in_collection(name, collection)
   result = {}
   collection.each do |item|
-    i.each do |k,v|
+    return item if
       if name == i[k]
         result = i
       end
