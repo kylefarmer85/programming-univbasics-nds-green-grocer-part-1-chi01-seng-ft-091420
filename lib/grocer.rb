777826e6b3089daa2423a6e7da960f95ab24d275
Kyle Farmer <kylefarmer85@gmail.com>
2020-09-01 19:22:17 +0000

    Done.

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,17 +1,28 @@
 require 'pry'
+# def find_item_by_name_in_collection(name, collection)
+#   result = {}
+#   collection.each do |i|
+#     i.each do |k,v|
+#       if name == i[k]
+#         result = i
+#       end
+#     end
+#   end
+#   if result == {}
+#     return nil
+#   end
+#   result
+# end
+
 def find_item_by_name_in_collection(name, collection)
-  result = {}
-  collection.each do |i|
-    i.each do |k,v|
-      if name == i[k]
-        result = i
-      end
-    end
+  index = 0
+
+  collection.each do |grocery_item|
+    return grocery_item if grocery_item[:item] === name 
+    index += 1
   end
-  if result == {}
-    return nil
-  end
-  result
+
+  nil
 end
  
 # def find_item_by_name_in_collection(name, collection)
@@ -41,6 +52,29 @@ def consolidate_cart(cart)
   new_cart
 end
 
+# OR
+
+def consolidate_cart(cart)
+  index = 0
+  new_cart = []
+  cart.each do |grocery_item|
+    current_item = find_item_by_name_in_collection(grocery_item[:item], new_cart)
+    if current_item
+      # new_cart_index = 0
+      new_cart.each do |new_cart_item|
+        if new_cart_item[:item] === current_item[:item]
+          new_cart_item[:count] += 1
+        end
+      #   new_cart_index += 1
+      end
+    else
+      grocery_item[:count] = 1
+      new_cart << grocery_item
+    end
+    index += 1
+  end
+  new_cart
+end
     
 # def consolidate_cart(cart)
 #   new_cart = []
@@ -63,7 +97,10 @@ end
 #   new_cart
 # end
   
-  
+
+
+
+
   
   
   
